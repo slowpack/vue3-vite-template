aeb5d81ae9032e401dbde77abc40a370a4c1f63a
wp <wp@gmail.com>
2023-04-07 10:52:50 +0800

    init noroute

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,14 @@
 <script setup lang="ts">
-import { readViewsDir } from '@/utils/readViews';
-import { useRouter } from 'vue-router';
-
-const files = readViewsDir(import.meta.glob('@/views/**/index.vue'))
-const router = useRouter()
-const toTheMoon = (menuItem: string) => {
-  router.push(menuItem)
-}
+import { ref } from 'vue'
+const msg = ref('Hello World')
 </script>
 
 <template>
-  <nav>
-    <menu v-for="item in files" @click="toTheMoon(item)">{{ item }}</menu>
-  </nav>
   <div>
-    <RouterView />
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
-<style scoped>
-nav {
-  height: 100vh;
-  width: 20vw;
-  display: flex;
-  flex-direction: column;
-}
-
-menu {
-  display: inline-block;
-  height: 10vh;
-  width: 10vw;
-  background: #ebb3b3;
-  text-align: center;
-}
+<style lang="less" scoped>
+  
 </style>
--- src/utils/readViews.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@/utils/readViews' {
-  export function readViewsDir(modules: { [path: string]: any }): string[];
-  }
